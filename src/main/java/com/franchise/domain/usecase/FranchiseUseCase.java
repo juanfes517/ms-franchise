5cf3bbea3b1989d114f34a018c7444a8609b6034
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-04-17 18:25:45 -0500

    feat: add create franchise methods to Franchise use case

---
+++ src/main/java/com/franchise/domain/usecase/FranchiseUseCase.java
@@ -0,0 +1,18 @@
+package com.franchise.domain.usecase;
+
+import com.franchise.domain.api.IFranchiseServicePort;
+import com.franchise.domain.model.Franchise;
+import com.franchise.domain.spi.IFranchisePersistencePort;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class FranchiseUseCase implements IFranchiseServicePort {
+
+    private final IFranchisePersistencePort franchisePersistencePort;
+
+    @Override
+    public Mono<Franchise> createFranchise(Franchise franchise) {
+        return franchisePersistencePort.save(franchise);
+    }
+}
