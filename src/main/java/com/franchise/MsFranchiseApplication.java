bf12dce1f89906875fae63db278c63a58892b602
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-04-17 17:10:30 -0500

    first commit

---
+++ src/main/java/com/franchise/MsFranchiseApplication.java
@@ -0,0 +1,22 @@
+package com.franchise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class MsFranchiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFranchiseApplication.class, args);
+	}
+
+	@GetMapping("/health")
+	public Mono<String> health() {
+		return Mono.just("OK");
+	}
+
+}
