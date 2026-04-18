63e96345e3b2b43a9a352707c8f20e7a15afeade
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-04-18 10:28:55 -0500

    feat: add bean configs

--- src/main/java/com/franchise/infrastructure/config/BeanConfig.java
+++ src/main/java/com/franchise/infrastructure/config/BeanConfig.java
@@ -1,7 +1,10 @@
 package com.franchise.infrastructure.config;
 
+import com.franchise.domain.api.IBranchServicePort;
 import com.franchise.domain.api.IFranchiseServicePort;
+import com.franchise.domain.spi.IBranchPersistencePort;
 import com.franchise.domain.spi.IFranchisePersistencePort;
+import com.franchise.domain.usecase.BranchUseCase;
 import com.franchise.domain.usecase.FranchiseUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +15,10 @@ public class BeanConfig {
     @Bean
     public IFranchiseServicePort franchiseServicePort(IFranchisePersistencePort franchisePersistencePort) {
         return new FranchiseUseCase(franchisePersistencePort);
+    }
+
+    @Bean
+    public IBranchServicePort  branchServicePort(IBranchPersistencePort branchPersistencePort) {
+        return new BranchUseCase(branchPersistencePort);
     }
 }
